baee2056db63dc276f9beff475226a75e4361e60
Annndreus <134499386+Annndreus@users.noreply.github.com>
2023-05-25 10:57:50 +0400

    Add files via upload

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,22 @@
+<center>
+    <a href="sample.html"><img src="https://mobimg.b-cdn.net/v3/fetch/93/93a40600c092a07aef65db88e06483b9.jpeg?w=1470&r=0.5625" width="355" 
+        height="200" alt="Пример"></a>
+</center>
+<h2 align="center">こんにちは, my friend, меня зовут Андрей</h2>
+<p align="left" class="priv"><b><i>Я не сильно интересный, но вполне здравый парень, учусь, занимаюсь спортом, имею свои планы на жизнь</i></b></p>
+<p align="left"><em><i><b>Мои увлечения:</b></i></em></p>
+<ul>
+    <li style="font-size: 18px;"><small>Баскетбол</small></li>
+    <li style="font-size: 18px;"><small>Скейтбординг</small></li>
+    <li style="font-size: 18px;"><small>Компьютерные игры</small></li>
+    <li style="font-size: 18px;"><small>Просмотр фильмов/сериалов</small></li>
+    <li style="font-size: 18px;"><small>Автоспорт</small></li>
+</ul>
+<style>
+    .priv  {
+        color: rebeccapurple;
+        margin: 60px;
+    }
+</style>
+
+
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,13 @@
+<h1 class="privet">Для того, чтобы связаться со мной:</h1>
+<ul>
+    <li><i style="font-size: 18px;"><b>dogen18rus@gmail.com</b></i></li>
+    <li><i style="font-size: 18px;"><b>+79127675217</b></i></li>
+</ul>
+<style>
+    .privet  {
+        color: red;
+        margin: 50px;
+    }
+</style>
+<a href="sample.html"><img src="https://itibsm.sfedu.ru/wp-content/uploads/2022/07/Kontakty-1-1536x768.jpg" width="600" 
+    height="300" alt="Пример"></a>
